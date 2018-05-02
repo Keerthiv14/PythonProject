fe949b4dbdcdf2c5e8e0841fcb4fb9211de2dd99
Keerthi V <Keerthi.v@lowes.com>
2018-05-02 12:57:56 +0530

    Python Project

---
+++ pack1/test_TC01_CreateIncident.py
@@ -0,0 +1,17 @@
+
+from selenium import webdriver
+
+ 
+
+browser =webdriver.Chrome()
+
+
+def test_open_url():
+    url='https://www.google.com/?safe=active&ssui=on'    
+    browser.get(url)
+    assert browser.current_url == url  
+
+
+def test_close_browser():
+    browser.close()
+    browser.quit()
